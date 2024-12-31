821459d55f1039810695e8a01c336bf950663a89
Jordan Muir <jordanhmuir@gmail.com>
2024-12-30 16:09:55 -0800

    refactored logger

--- server/job.py
+++ server/job.py
@@ -8,7 +8,6 @@ import uuid
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
-logger = logging.getLogger(__name__)
 
 
 class JobStatus(Enum):
@@ -23,20 +22,21 @@ class TranslationJob:
         self.start_time = time.time()
         self.processing_time = processing_time
         self.error_probability = error_probability
+        self.logger = logging.getLogger(__name__)
 
     def get_status(self) -> JobStatus:
         # Comparing the processing time to the actual time worked
         time_worked = time.time() - self.start_time
         # logger.info(f"Time Worked: {time_worked}, Processing Time: {self.processing_time}")
         if time_worked >= self.processing_time:
-            logger.info(f"Job status returned: {JobStatus.COMPLETED}")
+            self.logger.info(f"Job status returned: {JobStatus.COMPLETED}")
             return JobStatus.COMPLETED
 
         # Randomly causing an error if inside error threshold
         error = random.random()
         # logger.info(f"Error: {error}, Threshold:{self.error_probability}")
         if error < self.error_probability:
-            logger.info(f"Job status returned: {JobStatus.ERROR}")
+            self.logger.info(f"Job status returned: {JobStatus.ERROR}")
             return JobStatus.ERROR
         # If not completed or errored, return pending
         return JobStatus.PENDING
@@ -51,7 +51,7 @@ class TranslationServer:
         # Updating our jobs dictionary with a new job
         job = TranslationJob(processing_time, error_probability)
         self.jobs[job.job_id] = job
-        logger.info(f"Created TranslationJob, ID:{job.job_id}")
+        self.logger.info(f"Created TranslationJob, ID:{job.job_id}")
         return job.job_id
 
     def get_job_status(self, job_id: str) -> Optional[JobStatus]:
